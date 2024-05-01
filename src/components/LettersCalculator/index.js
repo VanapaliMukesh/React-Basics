49da144aca9fb6e404ea94f5e9f35ed672972871
mukesh <vanapallimukesh3@gmail.com>
2024-05-01 11:41:57 +0000

    solution

---
+++ src/components/LettersCalculator/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import './index.css'
+
+class LettersCalculator extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <h1>Mukesh</h1>
+      </div>
+    )
+  }
+}
+
+export default LettersCalculator
